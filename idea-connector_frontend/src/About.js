ce27ca86fa2e908e64a55a6984c0e9aa64149fbe
github username <adam.giegel@gmail.com>
2019-06-28 13:55:42 -0400

    06/29/19

--- idea-connector_frontend/src/About.js
+++ idea-connector_frontend/src/About.js
@@ -8,7 +8,7 @@ class About extends Component {
 render() {
   return (
       <div>
-        <div className="pic" style={{backgroundImage: 'url("https://digitalready.co/sites/default/files/styles/1000x427/public/best-innovative-and-creative-facebook-ads-from-famous-brands.jpg?itok=UB_QOW2l")'}}>
+        <div className="pic" style={{backgroundImage: 'url("https://seanwes.com/wp-content/uploads/2016/02/242-sp-find-your-why.png")'}}>
         <h1>Some about me stuff....Im not sure what will go here yet</h1>
         </div>
       </div>
