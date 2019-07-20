4daf23144fdbf379821fb7103cfe7cb1406a3207
github username <adam.giegel@gmail.com>
2019-07-20 17:46:33 -0400

    test

--- idea-connector_frontend/src/SelectedIdea.js
+++ idea-connector_frontend/src/SelectedIdea.js
@@ -78,6 +78,7 @@ class SelectedIdea extends Component{
       <Button class='ui button large' onClick={this.props.goBack}>GO BACK</Button>
       <Button class='ui button' onClick={() => this.props.deleteIdea(this.props.findIdea.id)}>DEAD IDEA</Button>
       <br></br>
+      <div>test</div>
       <br></br>
       </div>
     )
